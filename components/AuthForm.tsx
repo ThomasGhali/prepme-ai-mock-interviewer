db3f095b864ecc1ad2e9e06c96df9e9ffd5d2cdc
Thomas <thomasmetias29@gmail.com>
2025-12-05 00:45:47 +0200

    feat: Initialize project with authentication routes, shadcn/ui components, and updated asset management.

---
+++ components/AuthForm.tsx
@@ -0,0 +1,97 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
+import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
+import { Form } from '@/components/ui/form';
+
+import Logo from '@/public/logo.svg';
+import Image from 'next/image';
+
+import Link from 'next/link';
+import { toast } from 'sonner';
+import FormField from '@/components/FormField';
+
+const authFormSchema = (type: FormType) => {
+  return z.object({
+    name: type === 'sign-up' ? z.string().min(3) : z.string().optional(),
+    email: z.string().email(),
+    password: z.string().min(3),
+  });
+};
+
+const AuthForm = ({ type }: { type: FormType }) => {
+  const isSignIn = type === 'sign-in';
+
+  const formSchema = authFormSchema(type);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      if (type === 'sign-up') {
+        console.log('SIGN UP', values);
+      } else {
+        console.log('SIGN IN', values);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(`There was an error: ${error}`);
+    }
+  }
+
+  return (
+    <div className="card-border lg:min-w-[566px]">
+      <div className="flex flex-col gap-6 card py-14 px-10">
+        <div className="flex flex-row gap-2 justify-center">
+          <Image src={Logo} alt="Logo" />
+          <h2 className="text-primary-100">PrepMe</h2>
+        </div>
+
+        <h3 className="text-[1.2rem]">Nail your job interview with AI</h3>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-6 w-full mt-4 form"
+          >
+            {!isSignIn && (
+              <FormField
+                control={form.control}
+                name="name"
+                label="name"
+                placeholder="Your Name"
+              />
+            )}
+            <p>Email</p>
+            <p>Password</p>
+
+            <Button type="submit" className="btn">
+              {isSignIn ? 'Sign in' : 'Create an account'}
+            </Button>
+          </form>
+          <p className="text-center">
+            {isSignIn ? 'No account yet?' : 'Have an account already?'}
+            <Link
+              href={isSignIn ? '/sign-up' : '/sign-in'}
+              className="font-bold text-user-primary ml-1"
+            >
+              {isSignIn ? 'Sign up' : 'Sign in'}
+            </Link>
+          </p>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default AuthForm;
---
+++ components/FormField.tsx
@@ -0,0 +1,44 @@
+import {
+  FormControl,
+  FormDescription,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Control, Controller, FieldValues, Path } from 'react-hook-form';
+
+interface FormFieldProps<T extends FieldValues> {
+  control: Control<T>;
+  name: Path<T>;
+  label: string;
+  placeholder?: string;
+  type?: 'text' | 'email' | 'password' | 'file';
+}
+
+const FormField = ({
+  control,
+  name,
+  label,
+  placeholder,
+  type = 'text',
+}: FormFieldProps<T>) => {
+  return (
+    <Controller
+      control={control}
+      name="username"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel className='label'>Username</FormLabel>
+          <FormControl>
+            <Input placeholder="shadcn" {...field} />
+          </FormControl>
+          <FormDescription>This is your public display name.</FormDescription>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default FormField;
