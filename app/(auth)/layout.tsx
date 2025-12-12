b925d88e0c271cf07475e7e477e7ff788df0cde9
Thomas <thomasmetias29@gmail.com>
2025-12-12 21:36:01 +0200

    feat: Implement authentication checks and redirects in auth and root layouts based on user authentication status.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,12 +1,15 @@
-
+import { isAuthenticated } from '@/lib/actions/auth.action';
+import { redirect } from 'next/navigation';
 import { ReactNode } from 'react';
 
-const AuthLayout = ({ children }: { children: ReactNode }) => {
-  return (
-    <div className='auth-layout'>
-      {children}
-    </div>
-  )
+const AuthLayout = async ({ children }: { children: ReactNode }) => {
+  const isUserAuthinticated = await isAuthenticated();
+
+  if (isUserAuthinticated) {
+    return redirect('/');
+  }
+
+  return <div className="auth-layout">{children}</div>;
 };
 
 export default AuthLayout;
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,17 +3,25 @@ import Link from 'next/link';
 
 import Logo from '@/public/logo.svg';
 import Image from 'next/image';
+import { isAuthenticated } from '@/lib/actions/auth.action';
+import { redirect } from 'next/navigation';
 
-const HomeLayout = ({ children }: { children: ReactNode }) => {
+const HomeLayout = async ({ children }: { children: ReactNode }) => {
+  const isUserAuthinticated = await isAuthenticated();
+
+  if (!isUserAuthinticated) {
+    return redirect('/sign-in');
+  }
+
   return (
-    <div className='root-layout'>
+    <div className="root-layout">
       <nav>
         <Link href="/" className="flex items-center gap-2">
           <Image src={Logo} alt="Logo" />
           <h2 className="text-primary-100">PrepMe</h2>
         </Link>
       </nav>
-      
+
       {children}
     </div>
   );
